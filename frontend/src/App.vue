a80a943b24e35bda57bed970afa89cb274275ba7
USER <liam.croguennec@efrei.net>
2025-12-10 19:49:33 +0100

    Ajout du bouton NewProduct

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,8 +16,7 @@ function handleLogout() {
     <div class="nav-links">
       <router-link to="/products">Products</router-link>
       <router-link to="/cart">Cart</router-link>
-      
-      <router-link v-if="authStore.isAdmin()" to="/admin">Admin</router-link>
+
 
       <div v-if="authStore.isAuthenticated()" class="auth-menu">
         <span>Hello, {{ authStore.user?.nickname || 'User' }}</span>
